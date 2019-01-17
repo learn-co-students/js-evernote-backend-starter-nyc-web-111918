240b4d8ee8d30350c725b0f6faac9a2608b23e33
Tonyjlum <tonyjlum@gmail.com>
2019-01-17 11:43:03 -0500

    display notes whne clicked

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('ADD UR CODE HERE!')
+  const userUrl = "http://localhost:3000/api/v1/users/5"
+  const username = document.querySelector("#username")
+  const noteList = document.querySelector("#note-list")
+  const noteDisplay = document.querySelector("#note-display")
+
+  fetch(userUrl)
+  .then(response => response.json())
+  .then(userObj => pageSetUp(userObj))
+
+  function pageSetUp(userObj){
+    username.innerHTML = userObj.name
+    noteList.innerHTML = addNoteList(userObj)
+    noteDisplay.innerHTML = displayNote(userObj.notes[0])
+  }
+  function displayNote(noteObj){
+    let display = ``
+      display += `<h3>${noteObj.title}</h3>`
+      display += `<p>${noteObj.body}</p>`
+    return display
+  }
+
+  function addNoteList(userObj){
+    let noteLi = ``
+      userObj.notes.forEach(function(note){
+        noteLi += `<li data-note-id= ${note.id}> ${note.title}</li>`
+      })
+    return noteLi
+  }
+
+  noteList.addEventListener("click", function(event){
+    if (event.path[0].tagName == "LI"){
+      const noteId = event.target.dataset.noteId
+      fetch(`http://localhost:3000/api/v1/notes/${noteId}`)
+      .then(response => response.json())
+      .then(noteObj => noteDisplay.innerHTML = displayNote(noteObj))
+    }
+  })
+
+
 })
